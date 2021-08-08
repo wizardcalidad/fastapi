2dcbe3f3d1e2f41c055b3bac54e3d2f39072ba1c
wizardcalidad <yusufqoyum01@gmail.com>
2021-08-09 00:32:48 +0100

    fairphonics

---
+++ tests/test_fairphonics.py
@@ -0,0 +1,9 @@
+# As part of the CI, you will create a simple unit test
+# (very simple one, that checks 2 + 2 = 4 e.g.)
+import unittest
+
+class TestSum(unittest.TestCase):
+    def test_sum(self):
+        assert sum([2, 2]) == 4
+        print("Everything passed")
+
